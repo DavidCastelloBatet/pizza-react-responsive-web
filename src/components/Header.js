1fefc3e756207d29f13445420bd45588dfe41ac6
David Castello <davidcastellobatetgmail.com>
2020-12-05 18:26:45 +0100

    banner del Header

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="banner">
+      <div className="banner_content">
+        <div className="container">
+          <div className="banner_text">
+            <h3>Pizza Delivery</h3>
+            <h1>MAESTRO PIZINNI</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ad
+              harum maiores nulla debitis.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
