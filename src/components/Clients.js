216217147009928df6ee53a9bd6ce95bf6710166
David Castello <davidcastellobatetgmail.com>
2020-12-06 15:21:52 +0100

    refactoring clients saco datos a datos.js

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,18 +1,15 @@
 import "./Clients.css";
+import { clients } from "../datos";
 
 const Clients = () => {
-  const clients = [
-    { id: 1, num: 120, texto: "Awesome Employes" },
-    { id: 2, num: 58, texto: "Pizza Types" },
-    { id: 3, num: 1468, texto: "Satisfield Clients" },
-    { id: 4, num: 2031, texto: "Delivery" },
-  ];
+    
+  const clientes = clients;
 
   return (
     <div className="clients">
       <div className="container">
         <div className="row">
-          {clients.map((client) => {
+          {clientes.map((client) => {
             const { id, num, texto } = client;
             return (
               <div key={id} className="col-3">
---
+++ src/datos.js
@@ -0,0 +1,6 @@
+export const clients = [
+  { id: 1, num: 120, texto: "Awesome Employes" },
+  { id: 2, num: 58, texto: "Pizza Types" },
+  { id: 3, num: 1468, texto: "Satisfield Clients" },
+  { id: 4, num: 2031, texto: "Delivery" },
+];
